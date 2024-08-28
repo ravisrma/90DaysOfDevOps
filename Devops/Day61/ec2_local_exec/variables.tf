e893420db6191dc4f837b91e8f5e9b4aa21771d5
ravi <ravisharma0527@gmail.com>
2024-08-28 10:27:29 +0530

    remote-exec

---
+++ Devops/Day61/ec2_local_exec/variables.tf
@@ -0,0 +1,28 @@
+# variable "instance_id" {
+#   description = "The instance ID"
+#   type        = string
+# }
+variable "instance_type" {
+  description = "The instance type"
+  type        = string
+}
+variable "ami" {
+  description = "The AMI ID"
+  type        = string
+
+}
+variable "vpc_id" {
+  description = "The VPC ID"
+  type        = string
+
+}
+variable "ports_list" {
+  description = "The list of ports to open"
+  type        = list(number)
+
+}
+variable "instance_default_user" {
+  description = "The default user for the instance"
+  type        = string
+  
+}
